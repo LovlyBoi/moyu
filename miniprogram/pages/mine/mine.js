d2574a7af585511f894526fee330c5859e7ef245
LovlyBoi <728881636@qq.com>
2022-05-18 19:59:34 +0800

    反馈页面调整

--- miniprogram/pages/mine/mine.js
+++ miniprogram/pages/mine/mine.js
@@ -3,6 +3,7 @@ Page({
     itemList: ['不是所有的按钮都有用捏', '总有一些按钮庸庸碌碌的过完一生，却没有任何成就', '关于这个按钮没什么用但是我就想放在这这件事', '还想写一个但想不出来了'],
   },
   btn() {
+    // Toast(this.data.itemList[Math.floor(Math.random()*this.data.itemList.length)])
     wx.showToast({
       title: this.data.itemList[Math.floor(Math.random()*this.data.itemList.length)],
       icon: 'none',
